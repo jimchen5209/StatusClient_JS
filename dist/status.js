01b69d3f5386abf9c4868f8ae28cc018b7aa26fe
jimchen5209 <jimchen5209@gmail.com>
2021-09-01 16:04:52 +0800

    Update log logic
    Co-authored-by: WoodMan <WoodManGitHub@users.noreply.github.com>

--- dist/status.js
+++ dist/status.js
@@ -3,19 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Status = void 0;
 const os_1 = require("os");
 const fs_1 = require("fs");
+const child_process_1 = require("child_process");
 class Status {
     constructor(name) {
         this._path = (0, os_1.homedir)() + '/.bot_status';
+        const pid = process.pid;
+        const cmd = `ps -p ${pid} -ww -o args`;
+        const stdOut = (0, child_process_1.execSync)(cmd);
+        const cmdline = Buffer.from(stdOut).toString().split('\n')[1];
         this._data = {
-            'name': name,
-            'pid': process.pid,
-            'cmdline': process.argv,
-            'closed': false
+            name: name,
+            pid: pid,
+            cmdline: cmdline.split(' '),
+            closed: false,
+            pm2: process.argv[1].includes('pm2')
         };
         this._filename = name.replace(' ', '_');
     }
     set_status(closed = false) {
-        this._data['closed'] = closed;
+        this._data.closed = closed;
         if ((0, fs_1.existsSync)(this._path)) {
             (0, fs_1.writeFile)(`${this._path}/${this._filename}.json`, JSON.stringify(this._data), (err) => {
                 if (err)
